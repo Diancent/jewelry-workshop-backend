d09751416dc595a379d62e5d9e6b5acb5afe12c1
Diancent <artem.korol.jet@gmail.com>
2023-12-05 09:20:35 +0200

    Front for all tables

--- frontend/urls.py
+++ frontend/urls.py
@@ -12,12 +12,22 @@ urlpatterns = [
     path('customers/get/page', views.getCustomerPage, name='getCustomerPage'),
     path('customers/put/page', views.putCustomerPage, name='putCustomerPage'),
     path('customers/delete/page', views.deleteCustomerPage, name='deleteCustomerPage'),
+
     path('artisans/', views.artisans_page, name='artisans'),
+    path('artisans/create/page', views.createArtisanPage, name='createArtisanPage'),
+    path('artisans/get/page', views.getArtisanPage, name='getArtisanPage'),
+    path('artisans/put/page', views.putArtisanPage, name='putArtisanPage'),
+    path('artisans/delete/page', views.deleteArtisanPage, name='deleteArtisanPage'),
+
     path('materials/', views.materials_page, name='materials'),
+    path('materials/create/page', views.createMaterialPage, name='createMaterialPage'),
+    path('materials/get/page', views.getMaterialPage, name='getMaterialPage'),
+    path('materials/put/page', views.putMaterialPage, name='putMaterialPage'),
+    path('materials/delete/page', views.deleteMaterialPage, name='deleteMaterialPage'),
+
     path('products/', views.products_page, name='products'),
-    # path('customers', customers, name='getAllCustomers'),
-    # path('artisans', views.artisans_page, name='getAllArtisans'),
-    # path('materials', views.materials_page, name='getAllMaterials'),
-    # path('products', views.products_page, name='getAllProducts'),
-    # Додайте інші шляхи за потреби
+    path('products/create/page', views.createProductPage, name='createProductPage'),
+    path('products/get/page', views.getProductPage, name='getProductPage'),
+    path('products/put/page', views.putProductPage, name='putProductPage'),
+    path('products/delete/page', views.deleteProductPage, name='deleteProductPage'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -18,9 +18,33 @@ def deleteCustomerPage(request):
 
 def artisans_page(request):
     return render(request, 'artisans.html')
+def createArtisanPage(request):
+    return render(request, 'artisan/create_artisan.html')
+def getArtisanPage(request):
+    return render(request, 'artisan/get_artisan.html')
+def putArtisanPage(request):
+    return render(request, 'artisan/update_artisan.html')
+def deleteArtisanPage(request):
+    return render(request, 'artisan/delete_artisan.html')
 
 def materials_page(request):
     return render(request, 'materials.html')
+def createMaterialPage(request):
+    return render(request, 'material/create_material.html')
+def getMaterialPage(request):
+    return render(request, 'material/get_material.html')
+def putMaterialPage(request):
+    return render(request, 'material/update_material.html')
+def deleteMaterialPage(request):
+    return render(request, 'material/delete_material.html')
 
 def products_page(request):
     return render(request, 'products.html')
+def createProductPage(request):
+    return render(request, 'product/create_product.html')
+def getProductPage(request):
+    return render(request, 'product/get_product.html')
+def putProductPage(request):
+    return render(request, 'product/update_product.html')
+def deleteProductPage(request):
+    return render(request, 'product/delete_product.html')
